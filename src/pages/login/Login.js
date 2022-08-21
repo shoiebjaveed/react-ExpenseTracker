ee9bd1fd58307c922c2682b6321780b9bf96e27f
shoiebjaveed <shoiebjaveed@gmail.com>
2022-08-22 01:27:58 +0530

    Added validation for login

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -11,7 +11,40 @@ const Login = () => {
         event.preventDefault();
         const enteredEmail = email.current.value;
         const enteredPassword = password.current.value;
-        
+
+        setIsLoading(true)
+        fetch('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAhbVAuWXkewbZNP1a1KknskpNGB_F8deU',
+        {
+          method: 'POST',
+          body: JSON.stringify({
+            email: enteredEmail,
+            password: enteredPassword,
+            returnSecureToken: true
+          }),
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        }
+      ).then(res => {
+        setIsLoading(false)
+        if (res.ok) {
+          return res.json()
+        } else {
+          return res.json().then(data => {
+            let error = 'Login failed!...'
+            if (data && data.error && data.error.message) {
+              error = data.error.message;
+            }
+            alert(error)
+            throw new Error(error)
+          })
+        }
+      }).then(data => {
+        console.log(data)
+      })/*if sucessfull connection*/
+      .catch((err => {
+        alert(err.message)
+      }))//if connection failed
     }
 
   return (
@@ -23,11 +56,16 @@ const Login = () => {
         <input type='email' placeholder='Email' required ref={email}/>
         <label>Password</label>
         <input type='password' placeholder='password' required ref={password}/>
-        <button type='submit'>submit</button>
+        <button type='submit'>
+            {!isLoading && <p>submit</p>}
+            {isLoading && <p>please wait.....</p>}
+            </button>
       </form>
+      <div className={classes.condition}>
       <p>Don't Have an account? 
-        <Link to='/signup'> Signup</Link>
+        <Link to='/signup'>Signup</Link>
       </p>
+      </div>
     </div>
   </>
   )
